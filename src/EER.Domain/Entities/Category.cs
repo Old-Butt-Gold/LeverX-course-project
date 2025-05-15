40b22ceda6ee7324643a619f7f4f79fa573f00ec
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2025-05-15 21:59:22 +0300

    fix(Category entity): add two new fields

--- src/EER.Domain/Entities/Category.cs
+++ src/EER.Domain/Entities/Category.cs
@@ -6,4 +6,6 @@ public class Category : BaseEntity<int>
 {
     public string Name { get; set; }
     public string Description { get; set; }
+    public string Slug { get; set; }
+    public bool IsActive { get; set; } = true; // admin can hide/show category
 }
