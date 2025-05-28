8c1ba0cc7cb48261f97c824ddf011c93a5cd8f1e
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2025-05-28 22:06:37 +0300

    feat: add MongoDb project

---
+++ src/Persistence/EER.Persistence.MongoDB/Extensions/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EER.Persistence.MongoDB.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureMongo(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+
+    }
+}
