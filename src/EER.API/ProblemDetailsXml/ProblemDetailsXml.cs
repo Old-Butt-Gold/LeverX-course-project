8442cd32cca33d2a5d60136d221e0e13e803b83c
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2025-05-31 13:33:32 +0300

    feat: updated Global Exception Handler with ValidationException

---
+++ src/EER.API/ProblemDetailsXml/ProblemDetailsXml.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+
+namespace EER.API.ProblemDetailsXml;
+
+[XmlRoot("ProblemDetails")]
+public class ProblemDetailsXml
+{
+    public string? Type { get; set; }
+    public string? Title { get; set; }
+    public int? Status { get; set; }
+    public string? Detail { get; set; }
+    public string? Instance { get; set; }
+    public List<ExtensionEntry>? Extensions { get; set; }
+
+    [XmlArray("ValidationErrors")]
+    [XmlArrayItem("ValidationError")]
+    public List<ValidationErrorEntry>? ValidationErrors { get; set; }
+}
---
+++ src/EER.API/ProblemDetailsXml/ValidationErrorEntry.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+
+namespace EER.API.ProblemDetailsXml;
+
+public class ValidationErrorEntry
+{
+    [XmlAttribute("field")]
+    public string? Field { get; set; }
+
+    [XmlElement("Message")]
+    public string[]? Messages { get; set; }
+}
