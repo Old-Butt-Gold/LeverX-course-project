dfb2cfc925a512ddf35cd2573b1c20cee59b4fd1
Old-Butt-Gold <andrey2004andrey2021@gmail.com>
2025-06-03 20:18:38 +0300

    feat: Configured JWT

--- src/EER.API/Extensions/ServiceExtensions.cs
+++ src/EER.API/Extensions/ServiceExtensions.cs
@@ -1,7 +1,12 @@
 ﻿using System.Reflection;
+using System.Text;
 using EER.API.CustomAttributes;
 using EER.API.Filters;
+using EER.API.Settings;
 using EER.API.SwaggerSchemaFilters;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace EER.API.Extensions;
@@ -95,5 +100,37 @@ public static class ServiceExtensions
 
             options.SchemaFilter<EnumSchemaFilter>();
         });
+    }
+
+    public static void ConfigureJwt(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.Section));
+
+        var signingKey = configuration[$"{JwtSettings.Section}:SigningKey"];
+
+        if (string.IsNullOrWhiteSpace(signingKey))
+            throw new InvalidOperationException("JWT private key not configured");
+
+        services.AddAuthentication(opt =>
+            {
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidIssuer = configuration[$"{JwtSettings.Section}:Issuer"],
+                    ValidAudience = configuration[$"{JwtSettings.Section}:Audience"],
+                    NameClaimType = JwtRegisteredClaimNames.Sid,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey))
+                };
+            });
     }
 }
---
+++ src/EER.API/Settings/JwtSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace EER.API.Settings;
+
+public class JwtSettings
+{
+    public const string Section = "JwtSettings";
+    public string? ValidIssuer { get; init; }
+    public string? ValidAudience { get; init; }
+    public int ExpirySeconds { get; init; }
+    public int RefreshExpirySeconds { get; init; }
+    public string? SigningKey { get; init; }
+}
